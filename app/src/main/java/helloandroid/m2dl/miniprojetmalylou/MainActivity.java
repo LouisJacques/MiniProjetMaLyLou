319b56afbb04c162bee9fe995dd24eb705727f82
maxime <max3129@hotmail.fr>
2019-03-10 18:30:45 +0100

    màj (+ test de redimentionnement)

--- app/src/main/java/helloandroid/m2dl/miniprojetmalylou/MainActivity.java
+++ app/src/main/java/helloandroid/m2dl/miniprojetmalylou/MainActivity.java
@@ -1,54 +1,46 @@
 package helloandroid.m2dl.miniprojetmalylou;
 
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Vibrator;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.text.Layout;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
-import android.widget.ProgressBar;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener, SensorEventListener {
-    TextView tv;
-    private SensorManager sm = null;
-    private Handler mHandler;
-    private int cpt = 5;
+import java.io.IOException;
 
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, SensorEventListener {
+    // components
+    private SensorManager sm = null;
+    private MeasureDisplay display;
+    private MediaRecorder mRecorder;
+    private Handler mHandler;
+
+    // constants
+    private static final int VAL_MAX = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tv = new TextView(this);
-        tv.setText("Hi");
-
-        //tv.setOnTouchListener(this);
-        mHandler = new Handler();
-        //mHandler.postDelayed(mUpdateTimeTask, 1000);
-       // setContentView(tv);
-
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         setContentView(R.layout.activity_main);
-        View view = (View) findViewById(R.id.mainLayout);
-        // view.setText("pos x"  + "pos y" );
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        View view = findViewById(R.id.mainLayout);
+        /*LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
@@ -64,51 +56,116 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
             public void onProviderDisabled(String provider) {
             }
 
-        };
+        };*/
 
         view.setOnTouchListener(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    1);
-
-            return;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    10);
+            /*mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mRecorder.start(); */
+        } else {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mSleepTask.run();
         }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-        System.out.println("AAAAAAAAA" +locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
+        resizeLayout();
+        initDisplayer();
     }
-    private Runnable mUpdateTimeTask = new Runnable() {
+
+    private void resizeLayout() {
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+
+        LinearLayout l = findViewById(R.id.layout);
+        SurfaceView sv = findViewById(R.id.surfaceView);
+        Button btn = findViewById(R.id.buttonStop);
+
+        ViewGroup.LayoutParams params = l.getLayoutParams();
+        params.height = size.y;
+        params.width = size.x;
+
+        sv.setMinimumHeight(l.getHeight()-60);
+        sv.setMinimumWidth(l.getWidth()-2);
+
+        btn.setWidth(l.getWidth()-2);
+        btn.setHeight(60);
+    }
+
+    private void initDisplayer() {
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        display = new MeasureDisplay(size, (SurfaceView) findViewById(R.id.surfaceView));
+    }
+
+    private Runnable mSleepTask = new Runnable() {
         public void run() {
-
-            //if(cpt>1){
-                //tv.setText(Build.MODEL + cpt+"");
-                setContentView(tv);
-
-                //cpt--;
-                mHandler.postDelayed(this, 1000);
-
-            //}else{
-               // tv.setText("Hey");
-
-                //System.exit(RESULT_OK);
-                //finishActivity(RESULT_OK);
-
-            //}
-            sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-            //Handler mHandler = new Handler();
-            //mHandler.postDelayed(this, 1000);
-            //System.exit(RESULT_OK);
+            mRecorder.start();
+            //Noise monitoring start
+            // Runnable(mPollTask) will execute after POLL_INTERVAL
+            mHandler.postDelayed(eventSound, 1000);
         }
     };
+
+    private Runnable eventSound = new Runnable() {
+        public void run() {
+            Double amp = 20 * Math.log10(mRecorder.getMaxAmplitude() / 2700.0);
+            display.updatePtSonore(amp.intValue());
+            // Runnable(mPollTask) will again execute after POLL_INTERVAL
+            mHandler.postDelayed(eventSound, 1000);
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mRecorder = new MediaRecorder();
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                mRecorder.setOutputFile("/dev/null");
+                try {
+                    mRecorder.prepare();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //mRecorder.start();
+                mSleepTask.run();
+            } else {
+                //User denied Permission.
+            }
+        }
+    }
+
     @Override
     public void onSensorChanged(SensorEvent event) {
         int sensor = event.sensor.getType();
@@ -119,11 +176,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 float magField_x = values[0];
                 float magField_y = values[1];
                 float magField_z = values[2];
-                //TextView view = (TextView) findViewById(R.id.debugText);
-                ProgressBar progressbar = (ProgressBar) findViewById(R.id.progressBarAccel);
-                progressbar.setProgress((int) magField_z %100);
-                //view.setText("TYPE_MAGNETIC_FIELD x : " + magField_x +" y :"+magField_y+" z : "+magField_z);
-               // setContentView(view);
+                double strength = Math.sqrt( (magField_x*magField_x) + (magField_y*magField_y) + (magField_z*magField_z) );
+                int progress = (int) (strength) %VAL_MAX;
+
+                display.updatePtAccelerometre(Math.abs(progress));
             }
         }
     }
@@ -137,16 +193,13 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     public boolean onTouch(View v, MotionEvent event) {
         float posx = event.getX();
         float posy = event.getY();
-        cpt ++;
-        //TextView view = (TextView) findViewById(R.id.debugText);
-        //view.setText("pos x" + posx + "pos y" + posy);
-        ProgressBar progressbar = (ProgressBar) findViewById(R.id.progressBarTactile);
-        progressbar.setProgress((int) posx %100);
-        //setContentView(progressbar);
-        //setContentView(view);
+        int progress = (int) (posx*posy) %VAL_MAX;
 
+        display.updatePtEcran(progress);
         return false;
     }
+
+
 
     @Override
     protected void onResume() {
@@ -160,14 +213,9 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         sm.unregisterListener(this, sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
         super.onStop();
     }
+
     public void makeUseOfNewLocation(Location location){
-        ProgressBar progressbar = (ProgressBar) findViewById(R.id.progressBarGPS);
-        progressbar.setProgress((int) location.getLatitude() %100);
-        TextView view = (TextView) findViewById(R.id.debugText);
-        view.setText("Latitude : " + location.getLatitude() +" Longitude :"+location.getLongitude()+" Altitude : "+location.getAltitude());
-        setContentView(view);
-
-        System.out.println("loc has changed");
+        int progress = (int) (location.getLatitude()*location.getLongitude()) %VAL_MAX;
+        display.updatePtGPS(progress);
     }
-
 }
